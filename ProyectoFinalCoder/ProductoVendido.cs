e250d0b5889148e5c021d6a4527f7401c1b6727b
Igniske <ignaciomanuel07@gmail.com>
2024-02-01 09:28:08 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoFinalCoder/ProductoVendido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalCoder
+{
+    internal class ProductoVendido
+    {
+        private int _id;
+        private int _idProducto;
+        private int _stock;
+        private int _idVenta;
+    }
+}
---
+++ ProyectoFinalCoder/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalCoder
+{
+    internal class Usuario
+    {
+        private int _id;
+        private string _nombre;
+        private string _apellido;
+        private string _nombreUsuario;
+        private string _contrasenia;
+        private string _mail;
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contrasenia { get; set; }
+        public string Mail { get; set; }
+
+    }
+}
